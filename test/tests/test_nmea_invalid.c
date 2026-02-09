57b33b31c2346df5e14c24e0e601f50e01c178ca
Roos Catling-Tate <roos@catling-tate.net>
2026-02-09 08:36:39 -0500

    Add some more checks to test_nmea_invalid

--- test/tests/test_nmea_invalid.c
+++ test/tests/test_nmea_invalid.c
@@ -27,6 +27,7 @@ test_nmea_invalid (void)
 	char nmea_no_checksum[] = "$GPGLL,3953.88008971,N,10506.75318910,W,034138.00,A,D\r\n";
 	char nmea_gll_too_mony_fields[] = "$GPGLL,3953.88008971,N,10506.75318910,W,034138.00,A,D,,,,,*7A\r\n";
 	char nmea_bad_doubles[] = "$GPGLL,39q3.880fz971,N,10506.7z3ag91v,W,034138.00,A,D\r\n";
+	char nmea_gga_fix_quality_invalid[] = "$GPGGA,092750.000,5271.6902,N,00760.3382,W,0,8,1.03,61.7,M,55.2,M,,*79\r\n";
 
 	/* expect `rgpstk_nmea_message_load` to return 0, but nmea_valid to be false */
 	test_res = rgpstk_nmea_message_load(nmea_bad_checksum, sizeof(nmea_bad_checksum) - 1, &message);
@@ -61,7 +62,6 @@ test_nmea_invalid (void)
 	if (test_res) {
 		printf("test_nmea_message_load() returned with no checksum %d\n", test_res);
 		res = 5;
-		rgpstk_nmea_message_free(&message);
 		goto end;
 	}
 	if (!message.nmea_valid) {
@@ -82,7 +82,6 @@ test_nmea_invalid (void)
 	if (test_res) {
 		printf("test_nmea_message_load() returned with too many fields %d\n", test_res);
 		res = 8;
-		rgpstk_nmea_message_free(&message);
 		goto end;
 	}
 	test_res = rgpstk_nmea_gps_get_lat_long_gll(&message, &lat, &lon);
@@ -98,13 +97,27 @@ test_nmea_invalid (void)
 	if (test_res) {
 		printf("test_nmea_message_load() returned with bad double values %d\n", test_res);
 		res = 10;
-		rgpstk_nmea_message_free(&message);
 		goto end;
 	}
 	test_res = rgpstk_nmea_gps_get_lat_long_gll(&message, &lat, &lon);
 	if (test_res == 0) {
 		printf("rgpstk_nmea_gps_get_lat_long_gll did not fail invalid message.\n");
 		res = 11;
+		rgpstk_nmea_message_free(&message);
+		goto end;
+	}
+	rgpstk_nmea_message_free(&message);
+
+	test_res = rgpstk_nmea_message_load(nmea_gga_fix_quality_invalid, sizeof(nmea_gga_fix_quality_invalid) - 1, &message);
+	if (test_res) {
+		printf("test_nmea_message_load() returned with fix quality of invalid %d\n", test_res);
+		res = 12;
+		goto end;
+	}
+	test_res = rgpstk_nmea_gps_get_lat_long_gll(&message, &lat, &lon);
+	if (test_res == 0) {
+		printf("rgpstk_nmea_gps_get_lat_long_gll did not fail invalid message.\n");
+		res = 13;
 	}
 	rgpstk_nmea_message_free(&message);
 
